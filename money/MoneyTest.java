8e1a9a5458a891dbc39b6208844df667f7dd3a1d
yellowsman <sigesigeito@gmail.com>
2019-09-02 00:00:47 +0900

    p15 第2章 明白な実装 まで実行
    ふりかえり
    * 設計の問題点(今回は副作用)をテストコードに写し取り、その問題点のせいでテストが失敗するのを確認した
    * 空実装でさっさとコンパイルを通した
    * 正しいと思える実装をすぐに行い、テストを通した

--- money/MoneyTest.java
+++ money/MoneyTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
  * TODO: [ ] $5 + 10 CHF = $10 (レートが2:1の場合)
  * TODO: [x] $5 * 2 = $10
  * TODO: [ ] amount を privateにする
- * TODO: [ ] Dollarの副作用をどうする？
+ * TODO: [x] Dollarの副作用をどうする？
  * TODO: [ ]  Moneyの丸め処理をどうする？
  */
 
@@ -15,8 +15,9 @@ public class MoneyTest {
   @Test
   public void testMultiplication(){
     Dollar five = new Dollar(5);
-
-    five.times(2);
-    assertEquals(10, five.amount);
+    Dollar product = five.times(2);
+    assertEquals(10, product.amount);
+    product = five.times(3);
+    assertEquals(15, product.amount);
   }
 }
