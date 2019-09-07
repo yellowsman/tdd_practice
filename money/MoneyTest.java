dcfc437fc82570c771258c728706b6f0c110f586
yellowsman <sigesigeito@gmail.com>
2019-09-07 22:31:03 +0900

    p86 第12章 設計とメタファー
    ふりかえり
    * 大きいテスト($5 + 10 CHF)を分割して、進み具合がわかる小さいテスト($5 + $5)を作成した
    * これから行う計算のためのメタファーについて深く考えた
    * 前章で書いたテストを、新しいメタファーを使って書き直した
    * テストがコンパイルできるところまで早足で進んだ
    * テストを通した
    * 本当の実装を導くためのリファクタリングを楽しみにしつつ、少し不安も感じている

--- money/MoneyTest.java
+++ money/MoneyTest.java
@@ -5,21 +5,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * TODO: [ ] $5 + 10 CHF = $10 (レートが2:1の場合)
- * TODO: [x] $5 * 2 = $10
- * TODO: [x] amount を privateにする
- * TODO: [x] Dollarの副作用をどうする？
+ * TODO: [ ] $5 + $5 = $10
  * TODO: [ ] Moneyの丸め処理をどうする？
- * TODO: [x] equals()
  * TODO: [ ] hashCode()
  * TODO: [ ] nullとの等価性比較
  * TODO: [ ] 他のオブジェクトとの等価性比較
- * TODO: [x] 5 CHF * 2 = 10 CHF
- * TODO: [x] DollarとFrancの重複
- * TODO: [x] equalsの一般化
- * TODO: [x] timesの一般化
- * TODO: [x] FrancとDollarを比較する
- * TODO: [x] 通貨の概念
- * TODO: [x] testFrancMutiplicationを削除する？
  * TODO: [ ]
  */
 
@@ -41,5 +31,13 @@ public class MoneyTest {
   public void testCurrency() {
     assertEquals("USD", Money.dollar(1).currency());
     assertEquals("CHF", Money.franc(1).currency());
+  }
+  @Test
+  public void testSimpleAddition() {
+      Money five = Money.dollar(5);
+      Expression sum = five.plus(five);
+      Bank bank = new Bank();
+      Money reduced = bank.reduce(sum, "USD");
+      assertEquals(Money.dollar(10), reduced);
   }
 }
